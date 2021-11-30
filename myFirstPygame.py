321d1e9d434ed73329079ab310ed3c99207f9b53
Jason R <90394027+The1Riv123@users.noreply.github.com>
2021-11-30 13:28:29 -0500

    v0.4
    setup text. created rectangle to contain text

--- myFirstPygame.py
+++ myFirstPygame.py
@@ -1,4 +1,4 @@
-# ny First pyGame, Jason Rivers, 11/30/21 1:15pm  v0.3
+# ny First pyGame, Jason Rivers, 11/30/21 1:25pm  v0.4
 
 import pygame, sys
 from pygame.locals import *
@@ -17,3 +17,12 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255) 
 BROWN = (165, 42, 42)
+
+#Setup the fonts
+basicFont = pygame.font.sysfont(None, 48)
+
+#Setup the text
+text = basicFont.render('Hello,world!,' True. WHITE, BLUE)
+textRect = text.get_rect()
+textRect.centerx = windowSurface.get_rect().centerx
+textRect.centery = windowSurface.get_rect().centery
